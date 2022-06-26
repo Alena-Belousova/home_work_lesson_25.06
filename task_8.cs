12f5ec09667b1d239721abdb07d6e2f4c5498367
alena <solne4ka@inbox.ru>
2022-06-26 18:57:55 +0300

    добавила задачу 8

---
+++ task_8.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задача 8");
+int Resolt=1;
+int N = new Random().Next(1,100);//5
+    Console.WriteLine("значение N: " + N);//5
+while (Resolt<N)//1<5
+{
+if (Resolt%2==0)
+{
+    Console.Write(Resolt + " , ");
+}
+Resolt++;
+}
+
+
