db8eb26a49e929536a3e82ef9633888f68e83e67
alena <solne4ka@inbox.ru>
2022-06-26 18:44:22 +0300

    переименовала задачу 2, добавила задачу 4

---
+++ task_2.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Задача 2 работает");
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+int max = number1;
+if(number2 > max) 
+{Console.WriteLine(number2);
+}
+else{
+   Console.WriteLine("максимальное значение из введенных = " + max); 
+}
---
+++ task_4.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задача 4");
+Console.Write("введите число 1: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("введите число 2: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("введите число 3: ");
+int number3 = Convert.ToInt32 (Console.ReadLine());
+int max=number1;
+if(number2>max) max=number2;
+if (number3>max) max=number3;
+Console.WriteLine("максимум трех чисел= " + max);
