caad9c0fbde464339fe91ba2f9c627fa27883dcc
alena <solne4ka@inbox.ru>
2022-06-26 18:50:29 +0300

    добавлена задача 6

---
+++ task_6.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задача 6");
+Console.Write("Введите число: ");
+int chislo = Convert.ToInt32 (Console.ReadLine());
+if(chislo%2==0)
+{
+Console.WriteLine("Ответ ДА");
+}
+else
+{
+Console.WriteLine("Ответ НЕТ");
+}
